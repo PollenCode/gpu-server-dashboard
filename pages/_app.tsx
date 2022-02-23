2c5b663368012a8b37a996f60f49f51e3076acfa
Stijn Rogiest <reddusted@gmail.com>
2022-02-23 21:51:42 +0100

    Install FontAwesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,10 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+// Fix for Next.js https://fontawesome.com/docs/web/use-with/react/use-with
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
 import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, Button, ButtonGroup, CloseButton, Container, IconButton } from "@chakra-ui/react";
+import { faAddressCard } from "@fortawesome/free-regular-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 export default function Home() {
     return (
-        <Container pt={8}>
-            <Alert status="error">
+        <Container>
+            <Alert status="error" mt={8}>
                 <AlertIcon />
                 <AlertTitle mr={2}>Your browser is outdated!</AlertTitle>
                 <AlertDescription>Your experience may be degraded.</AlertDescription>
                 <CloseButton position="absolute" right="8px" top="8px" />
             </Alert>
-            {/* <ButtonGroup isAttached>
+            <ButtonGroup isAttached mt={8}>
                 <Button>This is a testing button</Button>
-                <IconButton icon={() => <>+</>}></IconButton>
-            </ButtonGroup> */}
+                <IconButton aria-label="Remove" icon={<FontAwesomeIcon icon={faTrash} />} />
+            </ButtonGroup>
         </Container>
     );
 }
