e9d09341230ccee76e6a62e28f5365c8327859a1
Stijn Rogiest <reddusted@gmail.com>
2022-02-23 21:38:25 +0100

    Update testing content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,15 @@
-import { Box } from "@chakra-ui/react";
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, CloseButton } from "@chakra-ui/react";
 import React from "react";
 
 export default function Home() {
-    return <Box fontWeight="bold">This is a test</Box>;
+    return (
+        <Box fontWeight="bold">
+            <Alert status="error">
+                <AlertIcon />
+                <AlertTitle mr={2}>Your browser is outdated!</AlertTitle>
+                <AlertDescription>Your experience may be degraded.</AlertDescription>
+                <CloseButton position="absolute" right="8px" top="8px" />
+            </Alert>
+        </Box>
+    );
 }
