32d198d19e38bd415f474da55d4e3f7ada30466d
Stijn Rogiest <reddusted@gmail.com>
2022-02-25 11:17:14 +0100

    feat: create basic page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,18 @@
-import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, Button, ButtonGroup, CloseButton, Container, IconButton } from "@chakra-ui/react";
+import {
+    Alert,
+    AlertDescription,
+    AlertIcon,
+    AlertTitle,
+    Box,
+    Button,
+    ButtonGroup,
+    CloseButton,
+    Container,
+    FormControl,
+    FormLabel,
+    IconButton,
+    Input,
+} from "@chakra-ui/react";
 import { faAddressCard } from "@fortawesome/free-regular-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,16 +21,21 @@ import React from "react";
 export default function Home() {
     return (
         <Container>
-            <Alert status="error" mt={8}>
-                <AlertIcon />
-                <AlertTitle mr={2}>Your browser is outdated!</AlertTitle>
-                <AlertDescription>Your experience may be degraded.</AlertDescription>
-                <CloseButton position="absolute" right="8px" top="8px" />
-            </Alert>
-            <ButtonGroup isAttached mt={8}>
-                <Button>This is a testing button</Button>
-                <IconButton aria-label="Remove" icon={<FontAwesomeIcon icon={faTrash} />} />
-            </ButtonGroup>
+            <Box p={4} mt={8} shadow="md" rounded="lg">
+                <form>
+                    <FormControl isRequired>
+                        <FormLabel>First name</FormLabel>
+                        <Input />
+                    </FormControl>
+                    <FormControl mt={4} isRequired>
+                        <FormLabel>Last name</FormLabel>
+                        <Input />
+                    </FormControl>
+                    <Button colorScheme="blue" mt={4} w="full">
+                        Submit
+                    </Button>
+                </form>
+            </Box>
         </Container>
     );
 }
