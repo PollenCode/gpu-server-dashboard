99da7418c457865604a9675ae008b2e46664c28c
Stijn Rogiest <reddusted@gmail.com>
2022-04-05 18:46:39 +0200

    feat: tasks will be scheduled in the first available spot when creating a task using the api

--- pages/api/task/index.ts
+++ pages/api/task/index.ts
@@ -1,6 +1,29 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { Task } from ".prisma/client";
+import next, { NextApiRequest, NextApiResponse } from "next";
 import { getSessionUser, getSessionUserId } from "../../../auth";
 import { prisma } from "../../../db";
+import { GPU_COUNT } from "../../../util";
+
+function findScheduleSpot(tasks: Task[], trainMilliseconds: number): Date {
+    if (tasks.length <= 0) {
+        // Schedule in 15 minutes
+        return new Date(new Date().getTime() + 1000 * 60 * 15);
+    } else {
+        // Find a spot between existing tasks
+        for (let i = 0; i < tasks.length - 1; i++) {
+            let now = tasks[i];
+            let next = tasks[i + 1];
+
+            if (now.endDate!.getTime() + trainMilliseconds < next.startDate!.getTime()) {
+                // Found a spot
+                return now.endDate!;
+            }
+        }
+
+        let last = tasks[tasks.length - 1];
+        return last.endDate!;
+    }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     let userId = await getSessionUserId(req, res);
@@ -34,6 +57,52 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     } else if (req.method === "POST") {
         let data = req.body;
 
+        let allGpus = !!data.allGpus;
+        let trainMilliseconds = 1000 * 60 * data.trainMinutes;
+
+        let nextTasks = await prisma.task.findMany({
+            where: {
+                startDate: {
+                    not: null,
+                    gte: new Date(),
+                },
+                endDate: {
+                    not: null,
+                },
+            },
+            orderBy: {
+                startDate: "asc",
+            },
+        });
+
+        let scheduleDate: Date;
+        let scheduleGpus = [] as number[];
+        if (allGpus) {
+            // Find a spot where all the gpus aren't used
+            scheduleDate = findScheduleSpot(nextTasks, trainMilliseconds);
+            scheduleGpus = new Array(GPU_COUNT).fill(0).map((_, i) => i);
+        } else {
+            // Find a spot on each specific gpu
+            let scheduleDateCandidates = new Array(GPU_COUNT);
+            for (let g = 0; g < GPU_COUNT; g++) {
+                let gpuSpecificScheduleDate = findScheduleSpot(
+                    nextTasks.filter((e) => e.gpus.includes(g)),
+                    trainMilliseconds
+                );
+                scheduleDateCandidates[g] = gpuSpecificScheduleDate;
+            }
+
+            // Use the earliest available spot
+            scheduleDate = scheduleDateCandidates[0];
+            for (let g = 1; g < scheduleDateCandidates.length; g++) {
+                let gpuSpecificScheduleDate = scheduleDateCandidates[g];
+                if (gpuSpecificScheduleDate.getTime() < scheduleDate.getTime()) {
+                    scheduleDate = gpuSpecificScheduleDate;
+                    scheduleGpus = [g];
+                }
+            }
+        }
+
         let task = await prisma.task.create({
             data: {
                 name: data.name,
@@ -42,6 +111,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
                         id: userId,
                     },
                 },
+                startDate: scheduleDate,
+                endDate: new Date(scheduleDate.getTime() + trainMilliseconds),
+                description: data.description,
+                gpus: scheduleGpus,
             },
         });
 
--- util.ts
+++ util.ts
@@ -1,5 +1,7 @@
 export const IS_DEV = process.env.NODE_ENV === "development";
 
+export const GPU_COUNT = parseInt(process.env.GPU_COUNT!);
+
 export const SERVER_URL = IS_DEV ? "http://localhost:3000" : "https://testgpu2.ikdoeict.net";
 
 export const fetcher = (url: string) => fetch(url).then((res) => res.json());
