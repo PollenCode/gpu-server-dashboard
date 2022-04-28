065a7d1d67660303092b9793998c887f42a35e2f
Laurent Aerens <laurent.aerens@student.odisee.be>
2022-04-28 14:04:14 +0000

    Revert "test: write simple fetch test and install isomorphic-fetch"
    This reverts commit 1030723a2e24562363df55674399118857d4436f

--- test/api.test.ts
+++
@@ -1,9 +0,0 @@
-import fetch from "isomorphic-fetch";
-
-it("Does an api test", async () => {
-    let res = await fetch("http://localhost:3000", {
-        method: "GET",
-    });
-
-    expect(res.ok).toBeTruthy();
-});
