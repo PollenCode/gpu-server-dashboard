e0698929d267a04227d667f54324a318bcfed0b9
Stijn Rogiest <reddusted@gmail.com>
2022-05-31 23:35:45 +0200

    Style home page with gpus and processor image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
 import { User } from ".prisma/client";
-import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, Button, ButtonGroup, Center, CloseButton, Code, Container } from "@chakra-ui/react";
+import {
+    Alert,
+    AlertDescription,
+    AlertIcon,
+    AlertTitle,
+    Box,
+    Button,
+    ButtonGroup,
+    Text,
+    Center,
+    CloseButton,
+    Code,
+    Container,
+    Grid,
+    GridItem,
+    Flex,
+} from "@chakra-ui/react";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { GetServerSideProps } from "next";
@@ -15,12 +31,59 @@ export default function Index(props: { user?: User }) {
         <Box bg="gray.100" minH="100vh">
             <NavBar />
             <Container as="main" maxW="container.lg">
-                <Center height="96" border="4px dotted" borderColor="gray.300" mt={8} rounded="2xl">
-                    <Alert status="info" maxW="400px" rounded="lg" shadow="sm">
-                        <AlertIcon />
-                        Here will be some general information about the current running job without being logged in.
-                    </Alert>
-                </Center>
+                <Grid mt={16} gap={16} templateColumns="repeat(3,1fr)" templateRows="300px 200px">
+                    <GridItem
+                        backgroundSize="contain"
+                        backgroundRepeat="no-repeat"
+                        backgroundPosition="center"
+                        backgroundImage="url(/nvidiaA6000.png)"></GridItem>
+                    <GridItem
+                        backgroundSize="contain"
+                        backgroundRepeat="no-repeat"
+                        backgroundPosition="center"
+                        backgroundImage="url(/nvidiaA6000.png)"></GridItem>
+                    <GridItem
+                        backgroundSize="contain"
+                        backgroundRepeat="no-repeat"
+                        backgroundPosition="center"
+                        backgroundImage="url(/xeon.png)"></GridItem>
+                    <GridItem justifyContent="center">
+                        <Center flexDirection="column">
+                            <Text fontSize="4xl" fontWeight="bold">
+                                Nvidia A6000
+                            </Text>
+                            <Text fontSize="4xl" fontWeight="semibold">
+                                100°C
+                            </Text>
+                            <Text fontSize="4xl" fontWeight="semibold">
+                                100%
+                            </Text>
+                        </Center>
+                    </GridItem>
+                    <GridItem justifyContent="center">
+                        <Center flexDirection="column">
+                            <Text fontSize="4xl" fontWeight="bold">
+                                Nvidia A6000
+                            </Text>
+                            <Text fontSize="4xl" fontWeight="semibold">
+                                100°C
+                            </Text>
+                            <Text fontSize="4xl" fontWeight="semibold">
+                                100%
+                            </Text>
+                        </Center>
+                    </GridItem>
+                    <GridItem>
+                        <Center flexDirection="column">
+                            <Text fontSize="4xl" fontWeight="bold">
+                                Intel Xeon
+                            </Text>
+                            <Text fontSize="4xl" fontWeight="semibold">
+                                X graden
+                            </Text>
+                        </Center>
+                    </GridItem>
+                </Grid>
             </Container>
         </Box>
     );
